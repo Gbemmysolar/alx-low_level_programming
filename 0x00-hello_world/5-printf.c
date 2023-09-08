5dea9898b9aba3b76fea5dee03ec29b8693876cd
gbemmysolar <gbemmysolar8@gmail.com>
2023-09-07 19:51:15 -0700

    git

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- *main- Entry point
+ * main - Entry point
  *
- *Return: Always O (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("with proper grammer, but the outcome is a piece of art,\n");
-	return (O);
+	return (0);
 }
