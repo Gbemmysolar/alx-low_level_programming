3801bd2e6640c632b8c61adabb3f8770b1ea31d4
gbemmysolar <gbemmysolar8@gmail.com>
2023-09-07 18:37:43 -0700

    hard

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include<stdio.h>
 /**
- * main - Entry point
+ * main- Entry point
  *
  *Return: Always O (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(O);}
+	return(O);
+}
