6bcf2292d9f58b05db0279263b3d53ebe5eccd8c
gbemmysolar <gbemmysolar8@gmail.com>
2023-09-07 18:26:15 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,9 @@
 /**
  * main - Entry point
  *
- * Return: Always O (Success)
+ *Return: Always O (Success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(O);
-}
+	return(O);}
