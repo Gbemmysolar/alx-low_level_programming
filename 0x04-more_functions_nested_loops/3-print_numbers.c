173549be7d966048aeea75228f70c763370a2afc
gbemmysolar <gbemmysolar8@gmail.com>
2023-09-15 13:42:21 -0700

    supper

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* print_numbers - print number from 0 to 9
+* print_numbers - print numbers from 0 to 9
 * Return: void
 */
 
@@ -10,6 +10,6 @@ void print_numbers(void)
 	int i;
 
 	for (i = 0 ; i < 10 ; i++)
-		'_putchar'(i + '0');
-	'_putchar('\n');
+		_putchar(i + '0');
+	_putchar('\n');
 }
