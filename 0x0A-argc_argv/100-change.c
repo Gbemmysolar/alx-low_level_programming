da18d73a14d75d2a3d2b6261e45169c389f6e70a
gbemmysolar <gbemmysolar8@gmail.com>
2023-10-04 08:22:12 -0700

    four n five

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -13,10 +13,10 @@
 int main(int argc, char *argv[])
 {
 	int num, j, result;
-	int coins[] = {25, 10, 5, 2,1};
+	int coins[] = {25, 10, 5, 2, 1};
 
 	if (argc != 2)
-	{ 
+	{
 		printf("Error\n");
 		return (1);
 	}
